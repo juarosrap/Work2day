165cee876dd2ff6308717c34a38f24e30e767d1e
juarosrap <jrosraposo@gmail.com>
2025-07-16 12:27:01 +0200

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://work2day.vercel.app", // o el subdominio exacto de Vercel que estás usando
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
